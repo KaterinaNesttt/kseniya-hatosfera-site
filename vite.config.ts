105a5d12d059fb869630345a85a28f73ac375d1e
KaterinaNesttt <f44wjhv8c9@privaterelay.appleid.com>
2026-02-21 19:37:15 +0000

    111

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/hatosfera-home-portal/", // <--- важливо для GitHub Pages
+ 
   server: {
     host: "::",
     port: 8080,
